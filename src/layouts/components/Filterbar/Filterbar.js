9661133fe82c00a3752db4abbf7a15f9b24d8560
trongdtq <167288708+trongdtq@users.noreply.github.com>
2025-01-17 01:40:16 +0700

    Update Filterbar.js

--- src/layouts/components/Filterbar/Filterbar.js
+++ src/layouts/components/Filterbar/Filterbar.js
@@ -3,6 +3,7 @@ import classNames from 'classnames/bind';
 import styles from './Filterbar.module.scss';
 import Search from './Search';
 import Button from '~/components/Button';
+import config from '~/config';
 
 const cx = classNames.bind(styles);
 
@@ -13,18 +14,18 @@ function Filterbar() {
       <Search />
 
       <div className={cx('pop-tags')}>
-        <Button
+        <Button      
           to={`${config.routes.search}?query=${encodeURIComponent('Oriental characters')}?type=all`}
           className={cx('pop-item')}
           square
         >
           Oriental characters
         </Button>
-        <Button to={`/search?query=${encodeURIComponent('illustration')}?type=all`} className={cx('pop-item')} square>
+        <Button to={`${config.routes.search}?query=${encodeURIComponent('illustration')}?type=all`} className={cx('pop-item')} square>
           illustration
         </Button>
         <Button
-          to={`/search?query=${encodeURIComponent('Visual inspiration')}?type=all`}
+          to={`${config.routes.search}?query=${encodeURIComponent('Visual inspiration')}?type=all`}
           className={cx('pop-item')}
           square
         >
